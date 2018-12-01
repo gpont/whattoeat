deba8b6f0d0988137e1dbf25934b8821b280eadf
davi9091 <davi9091@gmail.com>
2018-12-01 13:57:07 +0700

    Добавил кнопку меню и кнопки преференций, нужно только их переставить в нормальное место

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
-import { Layout, Input, Button } from 'antd';
+import { Layout, Input, Button, Menu } from 'antd';
 
 const { Header, Footer, Content } = Layout;
 
@@ -12,12 +12,31 @@ class App extends Component {
 				<Header
 					className='layout-header'
 				>
+                    <Button
+                        className={'menu-button'}
+                    >
+                        Меню
+                    </Button>
 					Header
 				</Header>
 				<Content
 					className='layout-content'
 				>
-					<Input
+                    <div className={'preferences-box'}>
+                        <Button
+                            shape={'circle'}
+                            className={'preference-button vegan'}
+                        ></Button>
+                        <Button
+                            shape={'circle'}
+                            className={'preference-button muslim'}
+                        ></Button>
+                        <Button
+                            shape={'circle'}
+                            className={'preference-button lactose'}
+                        ></Button>
+                    </div>
+                    <Input
 						placeholder={'Есть что поесть?'}
 					/>
 					<Button
