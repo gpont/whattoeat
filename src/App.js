ccd3a2e0610ab714781ef253c919e851bdb909d9
Александр <lflenylol@gmail.com>
2018-12-01 15:27:36 +0700

    card update

--- src/App.js
+++ src/App.js
@@ -54,9 +54,10 @@ class App extends Component {
 							</div>
 						</Card>
 						<Card
-							style={{ width: 300 }}
+							className={'food-card'}
+							style={{ width: 300, 'padding-bottom': 50 }}
 							cover={<img alt="example" src="https://www.recipetineats.com/wp-content/uploads/2016/02/Beef-Hamburgers_7-2.jpg" />}
-							actions={[<Icon type="like" />, <Icon type="edit" />, <Icon type="ellipsis" />]}
+							actions={[<Icon type="like" />, <Icon type="message" />, <Icon type="share-alt" />]}
 						>
 							<Meta
 								className={'food-avatar'}
@@ -64,6 +65,41 @@ class App extends Component {
 								title="Вкусный бургер"
 								description="Из макдака"
 							/>
+							<div className="order-wrap">
+								<Button
+									// shape={'circle'}
+									className={'order-button'}
+								>
+									Заказать
+								</Button>
+								<p className="price">
+									10 rub
+								</p>
+							</div>
+						</Card>
+						<Card
+							className={'food-card'}
+							style={{ width: 300, 'padding-bottom': 50 }}
+							cover={<img alt="example" src="https://www.recipetineats.com/wp-content/uploads/2016/02/Beef-Hamburgers_7-2.jpg" />}
+							actions={[<Icon type="like" />, <Icon type="message" />, <Icon type="share-alt" />]}
+						>
+							<Meta
+								className={'food-avatar'}
+								// avatar={<Avatar src="https://www.recipetineats.com/wp-content/uploads/2016/02/Beef-Hamburgers_7-2.jpg" />}
+								title="Вкусный бургер"
+								description="Из макдака"
+							/>
+							<div className="order-wrap">
+								<Button
+									// shape={'circle'}
+									className={'order-button'}
+								>
+									Заказать
+								</Button>
+								<p className="price">
+									10 rub
+								</p>
+							</div>
 						</Card>
 
 					</div>
